61cf5f7b48d957295389941b007cf31e05014752
NayanMallet <nayanmallet@gmail.com>
2025-08-07 16:21:48 +0200

    ✨ feat(security): add helmet and express-xss-sanitizer for enhanced security

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import dotenvFlow from 'dotenv-flow';
 import cors from 'cors';
+import helmet from 'helmet';
+import { xss } from 'express-xss-sanitizer';
+
 import connectDB from './config/connectDB';
 import { setupSwagger } from './config/swagger';
 
@@ -10,19 +13,31 @@ import protectedRoutes from './routes/protectedRoutes';
 import pollRoutes from './routes/pollRoutes';
 import responsesRoutes from "./routes/responsesRoutes";
 
-
 dotenvFlow.config();
 console.log('JWT_SECRET from dotenv-flow:', process.env.JWT_SECRET);
 
 const app = express();
-app.use(express.json());
-app.use(cors({ origin: process.env.CLIENT_URL || '*', credentials: true }));
 
+// Middlewares de sécurité
+app.use(helmet());
+app.use(xss());
+
+// Body parser
+app.use(express.json());
+
+// CORS sécurisé
+app.use(cors({
+    origin: process.env.CLIENT_URL || '*',
+    credentials: true,
+}));
+
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/protected', protectedRoutes);
 app.use('/api/polls', pollRoutes);
 app.use('/api/polls', responsesRoutes);
 
+// Server
 if (process.env.NODE_ENV !== 'test') {
     connectDB();
     setupSwagger(app);
