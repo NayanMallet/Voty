b6f546bed6f2873764dbb73845901210e16998b2
NayanMallet <nayanmallet@gmail.com>
2025-08-07 16:56:50 +0200

    :test_tube: test(jest): patch console.warn to silence dotenv-flow warnings globally

--- server/tests/jest.setup.ts
+++ server/tests/jest.setup.ts
@@ -4,14 +4,12 @@ process.env.JWT_SECRET = 'test_secret';
 import dotenvFlow from 'dotenv-flow';
 dotenvFlow.config({ node_env: 'test' });
 
-// Silence warnings spécifiques à dotenv-flow en CI
+// Silence warnings spécifiques à dotenv-flow
 const originalWarn = console.warn;
-beforeAll(() => {
-    console.warn = (...args) => {
-        if (
-            typeof args[0] === 'string' &&
-            args[0].includes('[dotenv-flow@')
-        ) return;
-        originalWarn(...args);
-    };
-});
+console.warn = (...args) => {
+    if (
+        typeof args[0] === 'string' &&
+        args[0].includes('[dotenv-flow@')
+    ) return;
+    originalWarn(...args);
+};
